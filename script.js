396cff9d18a397b276ed9824d8a530177ee9ecad
blindfox0 <mateusz.s@me.com>
2022-02-25 00:29:11 +0100

    Poprawki i ulepszenia
    Uporządkowano layout, ulepszono tworzenie tablicy kolorow, dodano przyciski do obslugi, dodano nowy rysunek, dodano alert przy probie kolorowania bez wybranego koloru.

--- script.js
+++ script.js
@@ -3,19 +3,46 @@ window.onload = function () {
     var shapes = document.getElementById("image");
     var colors = document.getElementById("colors");
     var chosenColor = document.getElementById("chosenColor");
-    var shape = image.children;
-    console.log(shape.length);
+    var clearColors = document.getElementById('clearColors');
+    var resumeColors = document.getElementById('resumeColors');
+    var colorToReplace;
+    var colorTempArray = [];
+    var colorBackup = [];
+    var shape = shapes.children;
+    console.log(colorToReplace);
 
+    /* funkcja pobiera kolory z obrazka SVG i wstawia go do tablicy kolorow. */
+    /* Sprawdza tez przed kazdym dodaniem koloru, czy nie ma go juz w tablicy - jesli jest, pomija go */
     function getColorsFromImage() {
         for (var i = 0; i < shape.length; i++) {
-            var colorDiv = document.createElement('div');
-            colorDiv.classList.add("color");
-            colorDiv.style.backgroundColor = shape[i].style.fill;
-            colors.appendChild(colorDiv);
+            var colorTemp = shape[i].style.fill;
+            colorBackup.push(colorTemp);
+
+            if (!(colorTempArray.includes(colorTemp)) && (colorTemp != '')) {
+                colorTempArray.push(colorTemp);
+                var colorDiv = document.createElement('div');
+                colorDiv.classList.add("color");
+                colorDiv.style.backgroundColor = colorTemp;
+                colors.appendChild(colorDiv);
+            }
         }
     }
 
     getColorsFromImage();
+
+     /* czyszczenie kolorow rysunku do zera, zeby mozna bylo malowac */
+    clearColors.onclick = function() {
+        for (var i = 0; i < shape.length; i++) {
+            shape[i].style.fill = '#fff';
+        }
+    }
+
+     /* przywrocenie kolorow do stanu poczatkowego */
+    resumeColors.onclick = function() {
+        for (var i = 0; i < shape.length; i++) {
+            shape[i].style.fill = colorBackup[i];
+        }
+    }
 
     var divChosenColor = document.createElement("div");
     divChosenColor.classList.add("chosenColor");
@@ -24,14 +51,19 @@ window.onload = function () {
     /* Funkcja ponizej ustawia kolor w kliknietym elemencie na obrazku svg */
     svgImage.onclick = async function (event) {
         var clickedElement = event;
-
-        clickedElement.target.style.fill = divChosenColor.style.backgroundColor;
+        if (colorToReplace != null) {
+            clickedElement.target.style.fill = colorToReplace;
+        } else {
+            alert('Wybierz jakis kolor :)');
+        }
     }
-    
+
     /* Funkcja ponizej sczytuje klikniety kolor i ustawia go jako wybrany kolor */
     colors.onclick = async function (event) {
         var clickedElement = event;
-
-        divChosenColor.style.backgroundColor = clickedElement.target.style.backgroundColor;
+        colorToReplace = clickedElement.target.style.backgroundColor;
+        divChosenColor.style.backgroundColor = colorToReplace;
+        console.log(colorToReplace);
     }
+
 }
